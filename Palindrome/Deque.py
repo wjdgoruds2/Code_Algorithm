d78652b152e22a7608043345e9be66de8289cfa2
wjdgoruds2 <48480825+wjdgoruds2@users.noreply.github.com>
2021-01-18 18:41:58 +0900

    Revert "2021-01-18"
    This reverts commit 4c5d8dfe

---
+++ Palindrome/Deque.py
@@ -0,0 +1,18 @@
+import collections
+from typing import Deque
+
+
+class Test:
+    def isPalindrome(self,s:str)->bool:
+        strs: Deque=collections.deque()
+        for char in s:
+            if(char.isalnum()):
+                strs.append(char.lower())
+        while(len(strs)>1):
+            if strs.popleft() != strs.pop():
+                return False
+        return True
+w1=Test()
+w2=Test()
+print(w1.isPalindrome("A man, a plan, a canal: Panama"))
+print(w2.isPalindrome("race a car"))
