1cfda620cf53fad564c332d332aee88dd98b32ae
wjdgoruds2 <48480825+wjdgoruds2@users.noreply.github.com>
2021-01-29 22:37:50 +0900

    2021-01-29

---
+++ Stack_Queue/Remove_duplicate_letters/recursive.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def removeDuplicateLetters(self,s:str)->str:
+        for char in sorted(set(s)):
+            suffix=s[s.index(char):]
+            if set(s) == set(suffix):
+                return char+self.removeDuplicateLetters(suffix.replace(char,''))
+        return ''
+
+t=Solution()
+print(t.removeDuplicateLetters("bcabc"))
