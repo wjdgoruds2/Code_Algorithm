f38660aae206b9a0544147837ac3bf338370c218
wjdgoruds2 <48480825+wjdgoruds2@users.noreply.github.com>
2021-07-03 03:14:06 +0900

    0702

--- programmers/메뉴리뉴얼.py
+++ programmers/메뉴리뉴얼.py
@@ -9,7 +9,7 @@ def solution(orders, course):
             for li in combinations(order,num):
                 res = ''.join(sorted(li))#############조합을 만들면서 정렬
                 per.append(res)
-        sorted_candidates = Counter(per).most_common() ########가장 높은 순으로
+        sorted_candidates = Counter(per).most_common()########가장 높은 순으로
         print(sorted_candidates)
         answer += [menu for menu, cnt in sorted_candidates if cnt > 1 and cnt == sorted_candidates[0][1]]
     return sorted(answer)
